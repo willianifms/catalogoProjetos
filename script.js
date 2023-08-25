78f2f81f57e426b498ce7ed6d25cc41c03024942
willianifms <williancardoso00004@gmail.com>
2023-08-25 14:56:02 -0400

    add animates

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const cryptoCard = document.getElementById("cryptoCard");
+const priceElement = document.getElementById("price");
 
 // Simulate fetching cryptocurrency data
 setTimeout(() => {
   cryptoCard.style.display = "block";
+  priceElement.style.animation = "pulse 1s infinite"; // Ativar animação de pulsação
 }, 1000);
